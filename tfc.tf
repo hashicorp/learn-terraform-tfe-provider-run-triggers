6d73947395d43a5abb0f869fb5c45c7a4b9d92dc
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-09-30 21:01:53 -0700

    Convert all into a data source

--- tfc.tf
+++ tfc.tf
@@ -1,3 +1,15 @@
+locals {
+  all_members = csvdecode(file("assets/all.csv"))
+}
+
+# Retrieve organization users from "all.csv"
+data "tfe_organization_membership" "all" {
+  for_each = { for all_members in local.all_members : all_members.email => all_members... }
+
+  organization = var.tfc_org
+  email        = each.key
+}
+
 # Create run trigger for [ K8s ] -> [ Consul ]
 resource "tfe_run_trigger" "k8s_consul" {
   workspace_id  = tfe_workspace.consul.id
