d15f397457d2e39708a52b0e8949c71bea070567
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-09-30 20:49:47 -0700

    Remove all.csv, guide assumes user already exists in TFC

--- tfc.tf
+++ tfc.tf
@@ -1,15 +1,3 @@
-locals {
-  all_members = csvdecode(file("assets/all.csv"))
-}
-
-# Add all users to organization
-resource "tfe_organization_membership" "all" {
-  for_each = { for all_members in local.all_members : all_members.email => all_members... }
-
-  organization = var.tfc_org
-  email        = each.key
-}
-
 # Create run trigger for [ K8s ] -> [ Consul ]
 resource "tfe_run_trigger" "k8s_consul" {
   workspace_id  = tfe_workspace.consul.id
